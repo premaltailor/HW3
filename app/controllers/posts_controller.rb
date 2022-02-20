660d308b6054cc7cb219ee9e35d1ae0b01d8b0f0
premaltailor <premal.tailor@kellogg.northwestern.edu>
2022-02-20 20:46:45 +0000

    posts controllers

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,19 @@
+class PostsController < ApplicationController
+
+    def show
+        @post = Post.find(params["id"])
+    end
+    
+    def new
+        @post = Post.new
+        @place = Place.find(params["place_id"])
+        @post.place_id = @place.id
+    end
+    
+    def create
+        @post = Post.new(params["contact"])
+        @post.save
+        redirect_to "/places/#{@post.place.id}"
+    end
+      
+end
